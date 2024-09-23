9254ddfdb310aeacf30c2b3b911618a6bd243e79
Pedro Lima <pedrozagolima@gmail.com>
2024-09-23 19:22:34 -0300

    feat: create function stopwatch

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -7,15 +7,15 @@
             <div class="column">
                 <div class="is-flex is-align-items-center is-justify-content-space-between">
                     <section>
-                        <strong>00:00:00</strong>
+                        <strong>{{  tempoDecorrido }}</strong>
                     </section>
-                    <button class="button">
+                    <button class="button" @click="iniciarCronometro">
                         <span class="icon">
                             <i class="fas fa-play"></i>
                         </span>
                         <span>play</span>
                     </button>
-                    <button class="button">
+                    <button class="button" @click="finalizarCronometro">
                         <span class="icon">
                             <i class="fas fa-stop"></i>
                         </span>
@@ -31,7 +31,28 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'FormularioPrincipal'
+    data() {
+        return {
+            tempoEmSegundos: 0
+        }
+    },
+    computed: {
+        tempoDecorrido () : string {
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substring(11, 19)
+        }
+    },
+    name: 'FormularioPrincipal',
+    methods: {
+        iniciarCronometro() {
+            setInterval(() => {
+                this.tempoEmSegundos += 1
+            }, 1000)
+        },
+
+        finalizarCronometro() {
+            console.log('Finalizando')
+        }
+    }
 })
 </script>
 
