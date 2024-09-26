7452c2fc4e246c2343a3d5b226046a4382be7af2
Pedro Lima <pedrozagolima@gmail.com>
2024-09-26 19:54:38 -0300

    feat: create a gobal state with vuewx and inject in 'Formulario.vue' component

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,8 +1,18 @@
 <template>
     <div class="box formulario">
         <div class="columns">
-            <div class="column is-8" role="form" area-label="Formulário para criação de uma nova tarefa">
+            <div class="column is-5" role="form" area-label="Formulário para criação de uma nova tarefa">
                 <input type="text" class="input" placeholder="Qual a tarefa você deseja iniciar?" v-model="descricao">
+            </div>
+            <div class="column is-3">
+                <div class="select">
+                    <select v-model="idProjeto">
+                        <option value="">Selecione o projeto</option>
+                        <option :value="projeto.id" v-for="projeto in projetos" :key="projeto.id">
+                            {{ projeto.nome }}
+                        </option>
+                    </select>
+                </div>
             </div>
             <div class="column">
                 <Temporizador @ao-temporizador-finalizado="finalizarTarefa" />
@@ -12,13 +22,16 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
 import Temporizador from './Temporizador.vue';
+import { useStore } from 'vuex';
+import { key } from '@/store/store';
 
 export default defineComponent({
     data() {
         return {
-            descricao: ''
+            descricao: '',
+            idProjeto: ''
         }
     },
     name: 'FormularioPrincipal',
@@ -27,12 +40,20 @@ export default defineComponent({
         finalizarTarefa(tempoDecorrido: number): void {
             this.$emit('aoSalvarTarefa', {
                 duracaoEmSegundos: tempoDecorrido,
-                descricao: this.descricao
+                descricao: this.descricao,
+                projeto: this.projetos.find(proj => proj.id == this.idProjeto)
             })
             this.descricao = ''
         }
     },
-    emits: ['aoSalvarTarefa']
+    emits: ['aoSalvarTarefa'],
+    setup() {
+        const store = useStore(key)
+
+        return {
+            projetos: computed(() => store.state.projetos)
+        }
+    }
 })
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import App from './App.vue'
 
 import '@fortawesome/fontawesome-free/css/all.css'
 import router from './routes/routes'
+import { key, store } from './store/store'
 
 createApp(App)
 .use(router)
+.use(store, key)
 .mount('#app')
