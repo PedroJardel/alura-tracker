c8e32bf40ad33972292371ded97224cf2f82bbf5
Pedro Lima <pedrozagolima@gmail.com>
2024-09-28 12:39:47 -0300

    feat: creates route nesting

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,5 @@
 import FormularioView from "@/views/Projetos/FormularioView.vue"
+import ListaView from "@/views/Projetos/ListaView.vue"
 import Projetos from "@/views/ProjetosView.vue"
 import Tarefas from "@/views/TarefasView.vue"
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router"
@@ -9,20 +10,27 @@ const routes: RouteRecordRaw[] = [
         path: '/',
         name: 'Tarefas',
         component: Tarefas
-    }, {
-        path: '/projetos',
-        name: 'Projetos',
-        component: Projetos
-    }, {
-        path: '/projetos/novo',
-        name: 'Novo Projeto',
-        component: FormularioView
-    }, {
-        path: '/projetos/:id',
-        name: 'Editar Projeto',
-        component: FormularioView,
-        props: true
     },
+    {
+        path: '/projetos',
+        component: Projetos,
+        children: [
+            {
+                path: '',
+                name: 'Projetos',
+                component: ListaView
+            }, {
+                path: 'novo',
+                name: 'Novo Projeto',
+                component: FormularioView
+            }, {
+                path: ':id',
+                name: 'Editar Projeto',
+                component: FormularioView,
+                props: true
+            },
+        ]
+    }
 ]
 
 const router = createRouter({
