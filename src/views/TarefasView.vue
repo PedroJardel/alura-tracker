9f25d21ee50a16d02f27874100a34269e62cc792
Pedro Lima <pedrozagolima@gmail.com>
2024-10-01 20:54:24 -0300

    feat: modal componentization

--- src/views/TarefasView.vue
+++ src/views/TarefasView.vue
@@ -1,13 +1,13 @@
 <template>
     <Formulario @aoSalvarTarefa="salvarTarefa" />
     <div class="field">
-            <p class="control has-icons-left has-icons-right">
-                <input class="input" type="email" placeholder="Digite para filtrar" v-model="filtro">
-                <span class="icon is-small is-left">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                </span>
-            </p>
-        </div>
+        <p class="control has-icons-left has-icons-right">
+            <input class="input" type="email" placeholder="Digite para filtrar" v-model="filtro">
+            <span class="icon is-small is-left">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </span>
+        </p>
+    </div>
     <div v-if="tarefas.length" class="lista">
 
         <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" :indexadorTarefa="index"
@@ -16,29 +16,27 @@
     <Box v-else class="lista-vazia">
         <h1><strong>Sua Lista de tarefas está vazia. Crie uma tarefa</strong></h1>
     </Box>
-    <div class="modal" :class="{ 'is-active': tarefaSelecionada }" v-if="tarefaSelecionada">
-        <div class="modal-background"></div>
-        <div class="modal-card">
-            <header class="modal-card-head">
-                <p class="modal-card-title">Editar</p>
-                <button class="delete" aria-label="close" @click="fecharModal"></button>
-            </header>
-            <section class="modal-card-body">
-                <div class="field">
-                    <label for="descricaoDaTarefa" class="label">
-                        Descrição
-                    </label>
-                    <input type="text" class="input" v-model="tarefaSelecionada.descricao" id="nomeDoProjeto">
-                </div>
-            </section>
-            <footer class="modal-card-foot">
-                <div class="buttons">
-                    <button class="button is-success" @click="alterarTarefa">Salvar</button>
-                    <button class="button" @click="fecharModal">Cancelar</button>
-                </div>
-            </footer>
-        </div>
-    </div>
+    <Modal :mostrar="tarefaSelecionada != null" v-if="tarefaSelecionada != null">
+        <template v-slot:cabecalho>
+            <p class="modal-card-title">Editar</p>
+            <button class="delete" aria-label="close" @click="fecharModal">
+            </button>
+        </template>
+        <template v-slot:corpo>
+            <div class="field">
+                <label for="descricaoDaTarefa" class="label">
+                    Descrição
+                </label>
+                <input type="text" class="input" v-model="tarefaSelecionada.descricao" id="nomeDoProjeto">
+            </div>
+        </template>
+        <template v-slot:rodape>
+            <div class="buttons">
+                <button class="button is-success" @click="alterarTarefa">Salvar</button>
+                <button class="button" @click="fecharModal">Cancelar</button>
+            </div>
+        </template>
+    </Modal>
 </template>
 
 <script lang="ts">
@@ -49,6 +47,7 @@ import Box from '../components/Box.vue';
 import { useStore } from '@/store/store';
 import { ALTERAR_TAREFA, CADASTAR_TAREFA, OBTER_PROJETOS, OBTER_TAREFAS } from '@/store/type-actions';
 import ITarefa from '@/interfaces/ITarefa';
+import Modal from '@/components/Modal.vue';
 
 export default defineComponent({
     name: 'TarefasView',
@@ -57,7 +56,7 @@ export default defineComponent({
             tarefaSelecionada: null as ITarefa | null
         }
     },
-    components: { Formulario, Tarefa, Box },
+    components: { Formulario, Tarefa, Box, Modal },
     methods: {
         salvarTarefa(tarefa: ITarefa): void {
             this.store.dispatch(CADASTAR_TAREFA, tarefa)
@@ -79,14 +78,12 @@ export default defineComponent({
         store.dispatch(OBTER_PROJETOS)
         const filtro = ref('')
 
-        // const tarefas = computed(() => store.state.tarefa.tarefas.filter((tarefaFiltrada) => !filtro.value || tarefaFiltrada.descricao.includes(filtro.value)))
-
         watchEffect(() => {
             store.dispatch(OBTER_TAREFAS, filtro.value)
         })
 
         return {
-            tarefas: computed (() => store.state.tarefa.tarefas),
+            tarefas: computed(() => store.state.tarefa.tarefas),
             store,
             filtro
         }
